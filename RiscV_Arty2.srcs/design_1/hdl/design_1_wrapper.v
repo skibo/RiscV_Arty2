af8b4728788ea861337c4caf84b213ed37dd9a85
Thomas Skibo <ThomasSkibo@sbcglobal.net>
2018-05-15 12:34:29 -0700

    Update to Vivado 2018.1.

--- RiscV_Arty2.srcs/design_1/hdl/design_1_wrapper.v
+++ RiscV_Arty2.srcs/design_1/hdl/design_1_wrapper.v
@@ -1,8 +1,8 @@
-//Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
-//Tool Version: Vivado v.2017.3 (lin64) Build 2018833 Wed Oct  4 19:58:07 MDT 2017
-//Date        : Sat Jan  6 20:32:21 2018
-//Host        : robleda running 64-bit Ubuntu 16.04.3 LTS
+//Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
+//Date        : Tue May 15 12:10:58 2018
+//Host        : clifford running 64-bit Ubuntu 16.04.4 LTS
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
 //Purpose     : IP block netlist
