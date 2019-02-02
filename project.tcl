0b151070623a9f82174da2dbeead7c5035b4d77f
Thomas Skibo <ThomasSkibo@yahoo.com>
2019-02-02 14:31:16 -0800

    Set default baud rate of debug uart.

--- project.tcl
+++ project.tcl
@@ -366,6 +366,9 @@ proc cr_bd_design_1 { parentCell } {
 
   # Create instance: axi_uartlite_1, and set properties
   set axi_uartlite_1 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_uartlite:2.0 axi_uartlite_1 ]
+  set_property -dict [ list \
+   CONFIG.C_BAUDRATE {115200} \
+ ] $axi_uartlite_1
 
   # Create instance: clk_wiz_0, and set properties
   set clk_wiz_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wiz:6.0 clk_wiz_0 ]
